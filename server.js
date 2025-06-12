51685ac9d0edbbf54e76b118ea022b404c656baf
hari-periasamy <hariharanperiasamy134@gmail.com>
2025-06-12 14:35:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+import express from 'express';
+import http from 'http';
+import { socketIo } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
